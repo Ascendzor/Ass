12b4b4ae2c6d9bac428a1b3cf22bd572eaa86164
tmare <bob.craker@hotmail.com>
2013-08-15 22:26:56 -0700

    homepage working)

---
+++ AuthenticationStorageSystem/EnlightenAss/Controllers/NavigateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnlightenAss.Controllers
+{
+    public class NavigateController : Controller
+    {
+        //
+        // GET: /Navigate/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
