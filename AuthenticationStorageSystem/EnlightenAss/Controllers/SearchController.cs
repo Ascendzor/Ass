bdd3466e237db82d17264b46788c6967240e8ba4
ZacJ <zacharyzor@gmail.com>
2013-08-17 03:15:34 +1200

    Better Coded Client only search(no input)

---
+++ AuthenticationStorageSystem/EnlightenAss/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AssClassLibrary;
+
+namespace EnlightenAss.Controllers
+{
+    public class SearchController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+        //
+        // GET: /Search/
+
+        public ActionResult Index(int id = 0)
+        {
+            return View(RelevantClients());
+        }
+        //
+        // GET: /Search/
+        
+        public List<Client> RelevantClients()
+        {
+          
+            List<Client> relevantClients = new List<Client>();
+            string searchString = "asd";
+            foreach (Client item in db.Clients)
+            {
+                if (item.Name.Contains(searchString))
+                {
+                    relevantClients.Add(item);
+
+                    ViewBag.test = item.ToString();
+                }
+            }
+            return relevantClients;
+        }
+
+    }
+}
