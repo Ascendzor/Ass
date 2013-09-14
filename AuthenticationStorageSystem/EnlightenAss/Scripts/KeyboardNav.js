f002b833fd3749e65591aab30701504810c5c33a
zacbil <abc.zacbil@gmail.com>
2013-09-14 17:03:47 +1200

    refactored keyboard nav

--- AuthenticationStorageSystem/EnlightenAss/Scripts/KeyboardNav.js
+++ AuthenticationStorageSystem/EnlightenAss/Scripts/KeyboardNav.js
@@ -1,15 +1,25 @@
-// JavaScript source code
+/*
+ * Keyboard navigation up and down table
+ * Table rows must have incremeneting IDs
+ * Enter invokes the selected elements onclick function
+ */
 
-function returnId(tempCounter) {
-    return document.getElementById(String(tempCounter));
-}
-
+// Global counter to identify what row is seleceted
 var counter = 0;
+
+/*
+ * Function is invoked when a partial view is loaded onto the page
+ * (from within the view html)
+ */
 function initialize() {
-    counter = 0;
-    returnId(0).className += " selectedRow";
+    $("#" + (counter = 0)).addClass("selectedRow");
 }
 
+/*
+ * Remove selected class from the row being unselected
+ * Add selected class to the row being selected
+ * Parameter 'x' will equal 1 to go up, -1 to go down
+ */
 function changeStyle(x) {
     $("#" + counter).removeClass("selectedRow");
     counter += x;
@@ -17,49 +27,32 @@ function changeStyle(x) {
 }
 
 /**
- * up key   = 38
- * down key = 40
- * enter    = 13
+ * up key   = 38 = changeStyle() + scroll window
+ * down key = 40 = changeStyle() + scroll window
+ * enter    = 13 = call onclick function of selected row
  */
 function handleKeyPressed(e) {
 
     switch(e.keyCode)
     {
-        case 13://enter
-            var loadElement = returnId(String(counter));
-            if (loadElement != null) {
-                loadElement.click();
-            }
+        case 13:
+            $("#" + counter).click();
             break;
-        case 38://up key
+        case 38:
             if (counter > 0) {
-                
                 changeStyle(-1);
-                window.scrollBy(0, document.getElementById(counter).offsetHeight * -1);
+                window.scrollBy(0, $("#" + counter).offsetHeight * -1);
             }
             e.preventDefault();
             break;
-        case 40://downkey
-            if (returnId(String(counter + 1)) != null) {
+        case 40:
+            if (document.getElementById(counter + 1) != null) {
                 changeStyle(1);
-                window.scrollBy(0, document.getElementById(counter).offsetHeight * 1);
-                
+                window.scrollBy(0, $("#" + counter).offsetHeight * 1);
             }
             e.preventDefault();
             break;
     }
 }
 
-function mousedOver(newCounter) {
-    changeStyle(newCounter - counter);
-    printElement(counter);
-}
-
-function printElement(element) {
-    console.log(element);
-    console.log(element.id);
-        
-}
-
-//window.onload = initialize;
 document.onkeydown = handleKeyPressed;
